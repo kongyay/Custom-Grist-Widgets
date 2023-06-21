5d154457465bc98c929743e531fb646f646d584f
Pitawat K <kongyay@hotmail.com>
2023-06-21 19:08:40 +0000

    feat: Basic update working

--- update-buttons/update-buttons.js
+++ update-buttons/update-buttons.js
@@ -1,61 +1,88 @@
-const { createApp, ref } = Vue
+const { createApp, ref, reactive } = Vue
 
 function setup() {
-    const status = ref('waiting');
-    const results = ref();
-    const input = reactive({
-        description: null,
-        button: null,
-        actions: null,
-    });
-
-    function handleError(err) {
-        console.error('ERROR', err);
-        status.value = String(err).replace(/^Error: /, '');
-    }
+    const BUTTONS_COLUMN = 'Buttons'
+    const TARGET_COLUMN = 'Target'
+    const tableId = ref();
+    const rowId = ref();
+    const colId = ref();
+    const errMsg = ref('');
+    const isLoading = ref(false);
+    const choices = ref([]);
+    const selectedValue = ref();
+    const currentValue = ref();
     
-    async function applyActions() {
-        results.value = "Working...";
-        try {
-            await grist.docApi.applyUserActions(input.value.actions);
-            message.value = 'Done';
-        } catch (e) {
-            message.value = `Please grant full access for writing. (${e})`;
+    async function doAction(index) {
+        if(!rowId.value || !colId.value || !tableId.value) {
+            errMsg.value = "Incomplete Ids"
+            return;
         }
-    }
-    
-    function onRecord(row, mappings) {
+        isLoading.value = true;
         try {
-            status.value = '';
-            results.value = null;
+            const choice = choices.value[index]
+            selectedValue.value = choice.value;
+            await grist.docApi.applyUserActions([ ['UpdateRecord', tableId.value, rowId.value, {
+                [colId.value]: choice.value
+              }]])
+        } catch (e) {
+            errMsg.value = `Please grant full access for writing. (${e})`;
+        }
+        isLoading.value = false;
+    }
+
+    grist.onRecord((row, mappings) => {
+        try {
+            rowId.value = row.id
+            errMsg.value = '';
+            isLoading.value = false;
+            if(!colId.value && !mappings?.[TARGET_COLUMN]) {
+                throw new Error(`Need a visible column named "${TARGET_COLUMN}".`);
+            }
+            colId.value = mappings[TARGET_COLUMN];
             // If there is no mapping, test the original record.
             row = grist.mapColumnNames(row) || row;
-            if (!row.hasOwnProperty(column)) {
-                throw new Error(`Need a visible column named "${column}". You can map a custom column in the Creator Panel.`);
+            if (!row[BUTTONS_COLUMN]) {
+                throw new Error(`Need a visible column named "${BUTTONS_COLUMN}".`);
             }
-            const keys = ['button', 'description', 'actions'];
-            if (!row[column] || keys.some(k => !row[column][k])) {
-                const allKeys = keys.map(k => JSON.stringify(k)).join(", ");
-                const missing = keys.filter(k => !row[column]?.[k]).map(k => JSON.stringify(k)).join(", ");
-                const gristName = mappings?.[column] || column;
-                throw new Error(`"${gristName}" cells should contain an object with keys ${allKeys}. ` +
-                    `Missing keys: ${missing}`);
+            const newChoices = row[BUTTONS_COLUMN];
+            if(!Array.isArray(newChoices)) {
+                throw new Error(`Column "${BUTTONS_COLUMN}" is not an array`);
             }
-            Object.assign(input, row[column]);
+            for(let i=0; i < newChoices.length; i+=1) {
+                const keys = ['label', 'value'];
+                const choice = newChoices[i]
+                if (!choice || keys.some(k => !choice[k])) {
+                    const allKeys = keys.map(k => JSON.stringify(k)).join(", ");
+                    const missing = keys.filter(k => !choice?.[k]).map(k => JSON.stringify(k)).join(", ");
+                    const buttonColumnId = mappings?.[BUTTONS_COLUMN] || BUTTONS_COLUMN;
+                    throw new Error(`
+                        "${buttonColumnId}" index=${i} should contain an object with keys ${allKeys}. 
+                        Missing keys: ${missing}.
+                    `);
+                }
+            }
+            choices.value = newChoices;
+            currentValue.value = row[TARGET_COLUMN];
         } catch (err) {
-            handleError(err);
+            console.error('ERROR', err);
+            errMsg.value = err;
         }
-    }
+    });
 
     // Update the widget anytime the document data changes.
-    grist.ready({ columns: [{ name: 'UpdateButtons', title: "UpdateButtons" }] });
-    grist.onRecord(onRecord);
+    grist.ready({ columns: [{ name: BUTTONS_COLUMN }, { name: TARGET_COLUMN }] });
+
+    grist.on('message', (e) => {
+        if (e.tableId) { tableId.value = e.tableId; }
+    });
 
     return {
-        status,
-        result,
-        input,
-        applyActions
+        errMsg,
+        isLoading,
+        choices,
+        selectedValue,
+        currentValue,
+        doAction
     }
 }
 
